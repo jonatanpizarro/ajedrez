9b0877f99307fe512f89c91bc507384e874d9eaa
JonatanPizarro <jonatan.pizarro1998@gmail.com>
2018-04-13 20:05:14 +0200

    wa

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -19,6 +19,5 @@ $factory->define(App\User::class, function (Faker $faker) {
         'email' => $faker->email,
         'password' => bcrypt(str_random(10)),
         'remember_token' => str_random(10),
-        'api_token' => str_random(50)
     ];
 });
