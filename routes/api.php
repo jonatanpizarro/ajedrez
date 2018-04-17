7bc5cee6a712a4210a156a236f2e89cdac78ae4f
JonatanPizarro <jonatan.pizarro1998@gmail.com>
2018-04-17 20:17:05 +0200

    blyat

--- routes/api.php
+++ routes/api.php
@@ -44,15 +44,16 @@ Route::get('/partida/{id_partida}' , function(){
 
 
 Route::get('/login/{name}/{password}' , function($name, $password){
-	if (Auth::attempt(['name'=> $name, 'password'=>$password])) {
+	if (Auth::attempt(['name'=> $name, 'password'=>$password, 'api_token'=>0])) {
 		
-		if (Auth::attempt(['name'=> $name, 'password'=>$password, 'api_token'=>0])){
+		if (Auth::attempt(['name'=> $name, 'password'=>$password])){
 			$rand_part = str_shuffle("abcdefghijklmnopqrstuvwxyz0123456789".uniqid());
 			
-			User::where('name',$name )					
+			User::where('name',$name )
 					->update(['api_token'=>$rand_part]);	
 
 			$user = User::where('name',$name )->get();
+
 			 header("Access-Control-Allow-Origin: *");
 			return json_encode($user);		
 		}
