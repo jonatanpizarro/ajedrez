9c69d817d94db195acfd3335906716b415400fe1
JonatanPizarro <jonatan.pizarro1998@gmail.com>
2018-05-02 19:52:03 +0200

    a

--- routes/api.php
+++ routes/api.php
@@ -175,7 +175,7 @@ Route::get('/mou/{jugador}/{id_partida}/{pos_ini}/{pos_dest}' , function($jugado
 	
 
 	header("Access-Control-Allow-Origin: *");	
-	return json_encode(array('estado'=>'ok','posIni'=>$posicion2[0],'posFin'=>$posicion ));
+	return json_encode(array('estado'=>'ok','posIni'=>$posicion2[0]['posicionIni'],'posFin'=>$posicion ));
 
 });
 
